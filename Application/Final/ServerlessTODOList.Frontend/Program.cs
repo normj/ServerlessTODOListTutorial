1b3eb649c7723bbc19e3d990c264e9db944a9a6d
Norm Johanson <normj@amazon.com>
2019-09-06 23:52:41 -0700

    Add Amazon.Extensions.Configuration.SystemsManager to final version

--- Application/Final/ServerlessTODOList.Frontend/Program.cs
+++ Application/Final/ServerlessTODOList.Frontend/Program.cs
@@ -19,6 +19,10 @@ namespace ServerlessTODOList.Frontend
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddSystemsManager("/ServerlessTODOList/");
+                })
                 .UseStartup<Startup>();
     }
 }
