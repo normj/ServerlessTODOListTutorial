e392e5e6e438ba8afe5f357ac0a6c1963464cffa
Norm Johanson <normj@amazon.com>
2019-09-07 15:24:14 -0700

    Add code to prepare for Lambda deployment

---
+++ Application/Final/ServerlessTODOList.Frontend/LambdaEntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace ServerlessTODOList.Frontend
+{
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .ConfigureAppConfiguration(configBuilder =>
+                {
+                    configBuilder.AddSystemsManager("/ServerlessTODOList/");
+                })
+                .UseStartup<Startup>();
+        }
+    }
+}
