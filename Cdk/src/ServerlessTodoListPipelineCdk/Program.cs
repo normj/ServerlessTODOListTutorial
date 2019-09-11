c8d7f33dca43f3b658ec9dce7caee90366c35b92
Norm Johanson <normj@amazon.com>
2019-09-11 00:05:25 -0700

    Add CDK project

---
+++ Cdk/src/ServerlessTodoListPipelineCdk/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ServerlessTodoListPipelineCdk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App(null);
+            new ServerlessTodoListPipelineCdkStack(app, "ServerlessTodoListPipelineCdkStack", new StackProps());
+            app.Synth();
+        }
+    }
+}
